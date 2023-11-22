cd659b0f6e662cad00c5229e37603c219204c23c
Soyoung Park <soyoung008@naver.com>
2023-11-22 16:03:51 +0900

    init : 테스트 코드 삭제

--- korean-air/src/App.tsx
+++ korean-air/src/App.tsx
@@ -1,20 +1,14 @@
 import GlobalStyle from './styles/GlobalStyle';
-import { styled, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Test>과연 적용은 ?</Test>
       </ThemeProvider>
     </>
   );
 }
 
 export default App;
-
-const Test = styled.p`
-  color: ${({ theme }) => theme.colors.blue};
-  ${({ theme }) => theme.fonts.title_bold_20};
-`;
