be70d400e6bfa656cf3e20256adc03875afadeaf
장정안 <wjddks96@naver.com>
2023-11-22 14:19:17 +0900

    chore: Eslint rules 추가
      	"@typescript-eslint/no-unused-vars": "warn",
    
        "react/prop-types": "off",
    
        "@typescript-eslint/no-explicit-any": "off",
    
    - 첫 번째 설정은 타입스크립트에서 사용하지 않은 변수를 감지하고 경고를 날려주는 설정입니다.
    
    말그대로 ‘경고’를 날리는 것이기 때문에 빌드에 실패하지 않습니다.
    
    - 두 번째 설정은 리액트에서 props의 type을 설정하는 것을 off하는 것입니다. 이 또한 추후 TS로 넘어갔을 때 규칙을 제거해도 좋을 것 같습니다
    
    - 마지막 세 번째 설정은 type에 any도 허용하는 것입니다.
    TS에서는 되도록 any를 피해야겠지만, JS에서 TS로 넘어가고 TS를 처음 접해보는 yb입장에서는,
    type에 any도 허용하면 어떨까 싶어 설정하였습니다.

--- korean-air/.eslintrc.cjs
+++ korean-air/.eslintrc.cjs
@@ -1,35 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "rules": {
-    }
-}
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
