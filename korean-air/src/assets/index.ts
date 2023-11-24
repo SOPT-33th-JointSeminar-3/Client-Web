337351d85b49e931205df26c1ac946836493ef2f
장정안 <wjddks96@naver.com>
2023-11-24 15:08:35 +0900

    add: icon 추가

--- korean-air/src/assets/index.ts
+++ korean-air/src/assets/index.ts
@@ -1,4 +1,6 @@
 import IcPaymentPlane from "./icon/ic_payment_plane.svg?react";
 import IcDownPayment from "./icon/ic_down_payment.svg";
+import IcPaymentShare from "./icon/ic_payment__share.svg";
+import IcPaymentSave from "./icon/ic_payment_save.svg";
 
-export { IcPaymentPlane, IcDownPayment };
+export { IcPaymentPlane, IcDownPayment, IcPaymentShare, IcPaymentSave };
