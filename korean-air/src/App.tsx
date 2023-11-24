e02eed1b6b568b3c1d1b3001d3170eba19cbc90f
lydiacho <sheecho0804@ewhain.net>
2023-11-24 16:05:27 +0900

    setting: 라우팅 세팅

--- korean-air/src/App.tsx
+++ korean-air/src/App.tsx
@@ -1,13 +1,13 @@
 import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
-import MainPage from "./pages/MainPage";
+import Router from "./Router";
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <MainPage />
+        <Router />
       </ThemeProvider>
     </>
   );
