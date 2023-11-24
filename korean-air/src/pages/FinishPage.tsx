374c27de0f2c74ae3ee1667a8f00000e01bdd458
lydiacho <sheecho0804@ewhain.net>
2023-11-25 00:55:52 +0900

    IcCloseSmall 추가

--- korean-air/src/pages/FinishPage.tsx
+++ korean-air/src/pages/FinishPage.tsx
@@ -1,7 +1,17 @@
 import styled from "styled-components";
+import { IcCloseSmall } from "../assets";
 
 const FinishPage = () => {
-  return <div>FinishPage</div>;
+  return (
+    <Wrapper>
+      <IcCloseSmall />
+    </Wrapper>
+  );
 };
 
 export default FinishPage;
+
+const Wrapper = styled.section`
+  width: 100%;
+  height: 100%;
+`;
