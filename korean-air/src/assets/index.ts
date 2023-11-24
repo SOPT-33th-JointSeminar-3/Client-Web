d5ccb45051607c46fdf9fd817bf3b5ab533a1387
lydiacho <sheecho0804@ewhain.net>
2023-11-24 18:36:45 +0900

    design: 사용할 icon import

--- korean-air/src/assets/index.ts
+++ korean-air/src/assets/index.ts
@@ -1,3 +1,5 @@
 import IcSwap from "./icon/ic_swap.svg?react";
+import IcCheckFinish from "./icon/ic_check_finish.svg";
+import IcCloseSmall from "./icon/ic_close_small.svg";
 
-export { IcSwap };
+export { IcSwap, IcCheckFinish, IcCloseSmall };
