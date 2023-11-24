8a2644ee1398024aa7ac30f81a960bc936a3e839
lydiacho <sheecho0804@ewhain.net>
2023-11-25 02:21:50 +0900

    chore: SVGR ?react 추가

--- korean-air/src/assets/index.ts
+++ korean-air/src/assets/index.ts
@@ -1,5 +1,5 @@
 import IcSwap from "./icon/ic_swap.svg?react";
-import IcCheckFinish from "./icon/ic_check_finish.svg";
-import IcCloseSmall from "./icon/ic_close_small.svg";
+import IcCheckFinish from "./icon/ic_check_finish.svg?react";
+import IcCloseSmall from "./icon/ic_close_small.svg?react";
 
 export { IcSwap, IcCheckFinish, IcCloseSmall };
