e6df66922758eb7b14854b655651230c424f1f0b
장정안 <wjddks96@naver.com>
2023-11-26 21:11:44 +0900

    feat: paymentPage 라우터 처리

--- korean-air/src/Router.tsx
+++ korean-air/src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import PaymentPage from "./pages/PaymentPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
       </Routes>
     </BrowserRouter>
   );
