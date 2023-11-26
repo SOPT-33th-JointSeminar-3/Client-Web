9b3aab0926dec4e2a6f8c01a706a81aa10b4e59c
장정안 <wjddks96@naver.com>
2023-11-26 21:12:34 +0900

    refactor: svg 파일 사용법 수정
    ?react를 붙여주었고, 컴포넌트 사용하듯이 사용하는 방법으로 변경하였습니다.

--- korean-air/src/assets/index.ts
+++ korean-air/src/assets/index.ts
@@ -1,9 +1,9 @@
 import IcPaymentPlane from "./icon/ic_payment_plane.svg?react";
-import IcDownPayment from "./icon/ic_down_payment.svg";
-import IcPaymentShare from "./icon/ic_payment__share.svg";
-import IcPaymentSave from "./icon/ic_payment_save.svg";
-import IcPaymentShotarrow from "./icon/ic_payment_shotarrow.svg";
-import LogoJinair from "./icon/logo_jinair.svg";
+import IcDownPayment from "./icon/ic_down_payment.svg?react";
+import IcPaymentShare from "./icon/ic_payment__share.svg?react";
+import IcPaymentSave from "./icon/ic_payment_save.svg?react";
+import IcPaymentShotarrow from "./icon/ic_payment_shotarrow.svg?react";
+import LogoJinair from "./icon/logo_jinair.svg?react";
 export {
   IcPaymentPlane,
   IcDownPayment,
