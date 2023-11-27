29def1f38ea36e0bbea4e90a1279bc18e198f16e
Soyoung Park <soyoung008@naver.com>
2023-11-28 00:31:00 +0900

    design: 프로모션 카드 왼쪽, 오른쪽 버튼 클릭으로 가로 스크롤 구현

--- korean-air/src/components/mainPage/Promotion.tsx
+++ korean-air/src/components/mainPage/Promotion.tsx
@@ -1,21 +1,24 @@
 import styled from "styled-components";
 import { PromotionCard } from "./PromotionCard";
 import { IcPagingLeft, IcPagingRight } from "../../assets";
+import { useState } from "react";
 
 export const Promotion = () => {
+  const [scrollLeft, setScrollLeft] = useState(true);
+
   return (
     <PromotionBox>
-      <IcPagingLeft />
-      <PromotionCard />
-      <IcPagingRight />
+      <IcPagingLeft onClick={() => setScrollLeft(true)} />
+      <PromotionCardBox $scroll={scrollLeft}>
+        <PromotionCard />
+      </PromotionCardBox>
+      <IcPagingRight onClick={() => setScrollLeft(false)} />
     </PromotionBox>
   );
 };
 
 const PromotionBox = styled.section`
-  display: flex;
   position: relative;
-  gap: 2.2rem;
   overflow-x: hidden;
   padding: 4rem 0 3.9rem 1rem;
 
@@ -31,4 +34,11 @@ const PromotionBox = styled.section`
     right: 0;
     z-index: 1;
   }
+`;
+
+const PromotionCardBox = styled.div`
+  display: flex;
+  gap: 2.2rem;
+  ${({ $scroll }) =>
+    $scroll ? "transform: translateX(0);" : "transform: translateX(-33.7rem);"};
 `;
