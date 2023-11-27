92129b2bb10e502218bbc1554b15f18c440918b4
장정안 <wjddks96@naver.com>
2023-11-28 06:58:28 +0900

    feat: header 컴포넌트 추가

--- korean-air/src/pages/PaymentPage.tsx
+++ korean-air/src/pages/PaymentPage.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import PaymentInfo from "../components/paymentPage/PaymentInfo";
 import IconLayout from "../components/paymentPage/IconLayout";
 import Journey from "../components/paymentPage/Journey";
+import Header from "../components/@common/Header";
 
 const PaymentPage = () => {
   return (
     <>
+      <Header />
       <PaymentInfo />
       <Journey />
     </>
