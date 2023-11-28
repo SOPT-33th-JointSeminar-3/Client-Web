c84835148a7b56a1f1804b7692fead6a9d227f15
lydiacho <sheecho0804@ewhain.net>
2023-11-28 16:37:23 +0900

    init: calendar 페이지 라우팅

--- korean-air/src/Router.tsx
+++ korean-air/src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import FinishPage from "./pages/FinishPage";
 import PaymentPage from "./pages/PaymentPage";
+import CalendarPage from "./pages/CalendarPage";
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/finish" element={<FinishPage />} />
         <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ korean-air/src/pages/CalendarPage.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const CalendarPage = () => {
+  return <div>CalendarPage</div>;
+};
+
+export default CalendarPage;
