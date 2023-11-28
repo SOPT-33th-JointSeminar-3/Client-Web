30c89c0220579b2b9c93843aaac4e6b7ffeee5b2
Soyoung Park <soyoung008@naver.com>
2023-11-28 11:53:02 +0900

    design : 컴포넌트 생성 및 SearchHeader 구현

---
+++ korean-air/src/components/searchPage/SearchHeader.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { IcCloseSmall } from "../../assets";
+const SearchHeader = (props) => {
+  const title = props.state === "departure" ? "출발지 검색" : "도착지 검색";
+  return (
+    <ShHeader>
+      <p>{title}</p>
+      <IcCloseSmall />
+    </ShHeader>
+  );
+};
+
+export default SearchHeader;
+
+const ShHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1.8rem 0 3.8rem 0;
+  & p {
+    ${({ theme }) => theme.fonts.title_bold_20};
+  }
+`;
--- korean-air/src/pages/SearchPage.tsx
+++ korean-air/src/pages/SearchPage.tsx
@@ -1,8 +1,20 @@
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import SearchHeader from "../components/searchPage/SearchHeader";
 const SearchPage = () => {
   const { state } = useLocation();
 
-  return <div></div>;
+  return (
+    <SearchBox>
+      <SearchHeader state={state}></SearchHeader>
+    </SearchBox>
+  );
 };
 
 export default SearchPage;
+
+const SearchBox = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 0 1.6rem;
+`;
