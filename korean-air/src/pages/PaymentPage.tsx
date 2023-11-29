23c4637cc05327641c6159cb03d7f30c2205c635
장정안 <wjddks96@naver.com>
2023-11-29 16:39:14 +0900

    feat: Footer 구현

--- korean-air/src/pages/PaymentPage.tsx
+++ korean-air/src/pages/PaymentPage.tsx
@@ -3,6 +3,9 @@ import PaymentInfo from "../components/paymentPage/PaymentInfo";
 import IconLayout from "../components/paymentPage/IconLayout";
 import Journey from "../components/paymentPage/Journey";
 import Header from "../components/@common/Header";
+import UserInfo from "../components/paymentPage/UserInfo";
+import CardPersonal from "../components/paymentPage/CardPersonal";
+import Footer from "../components/@common/Footer";
 
 const PaymentPage = () => {
   return (
@@ -10,6 +13,9 @@ const PaymentPage = () => {
       <Header />
       <PaymentInfo />
       <Journey />
+      <UserInfo />
+      <CardPersonal />
+      <Footer />
     </>
   );
 };
