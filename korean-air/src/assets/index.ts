b35d39a375552ec91978b1cc810d51a5a1d4f106
장정안 <wjddks96@naver.com>
2023-11-29 17:38:00 +0900

    add : svg 파일 추가

--- korean-air/src/assets/index.ts
+++ korean-air/src/assets/index.ts
@@ -30,6 +30,9 @@ import IcCloseSmallRecent from "./icon/ic_close_small_recnt.svg?react";
 import IcDownSearch from "./icon/ic_down_search.svg?react";
 import IcPin from "./icon/ic_pin.svg?react";
 import IcCloseVerySmall from "./icon/ic_close_verysmall.svg?react";
+import IcRightFlight from "./icon/ic_right_flight.svg?react";
+import IcLeftFlight from "./icon/ic_left_flight.svg?react";
+
 export {
   IcCalendar,
   IcHelp,
@@ -63,4 +66,6 @@ export {
   IcDownSearch,
   IcPin,
   IcCloseVerySmall,
+  IcRightFlight,
+  IcLeftFlight,
 };
