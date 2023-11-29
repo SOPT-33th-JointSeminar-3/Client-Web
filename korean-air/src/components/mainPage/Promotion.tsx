5e7cabc17ebd2215c0e1ed8577cf9a9d5318a037
Soyoung Park <soyoung008@naver.com>
2023-11-29 09:43:53 +0900

    refactor : svg div로 감싸서 onClick 이벤트 줌

--- korean-air/src/components/mainPage/Promotion.tsx
+++ korean-air/src/components/mainPage/Promotion.tsx
@@ -8,11 +8,15 @@ export const Promotion = () => {
 
   return (
     <PromotionBox>
-      <IcPagingLeft onClick={() => setScrollLeft(true)} />
+      <LeftBtn onClick={() => setScrollLeft(true)}>
+        <IcPagingLeft />
+      </LeftBtn>
       <PromotionCardBox $scroll={scrollLeft}>
         <PromotionCard />
       </PromotionCardBox>
-      <IcPagingRight onClick={() => setScrollLeft(false)} />
+      <RightBtn onClick={() => setScrollLeft(false)}>
+        <IcPagingRight />
+      </RightBtn>
     </PromotionBox>
   );
 };
@@ -21,19 +25,6 @@ const PromotionBox = styled.section`
   position: relative;
   overflow-x: hidden;
   padding: 4rem 0 3.9rem 1rem;
-
-  & svg:nth-child(1) {
-    position: absolute;
-    top: 35%;
-    left: 0;
-    z-index: 1;
-  }
-  & svg:nth-last-child(1) {
-    position: absolute;
-    top: 35%;
-    right: 0;
-    z-index: 1;
-  }
 `;
 
 const PromotionCardBox = styled.div<{ $scroll: boolean }>`
@@ -41,4 +32,25 @@ const PromotionCardBox = styled.div<{ $scroll: boolean }>`
   gap: 2.2rem;
   ${({ $scroll }) =>
     $scroll ? "transform: translateX(0);" : "transform: translateX(-29.7rem);"};
+`;
+
+const PagingBtn = styled.div`
+  position: absolute;
+  top: 35%;
+  width: 2.4rem;
+  height: 4.9rem;
+`;
+
+const LeftBtn = styled(PagingBtn)`
+  left: 0;
+  z-index: 1;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const RightBtn = styled(PagingBtn)`
+  right: 0;
+  z-index: 1;
+  display: flex;
+  justify-content: flex-end;
 `;
