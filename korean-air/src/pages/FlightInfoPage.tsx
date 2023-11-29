425c7439f697674ceeb0f2fbc857e81308534d8b
장정안 <wjddks96@naver.com>
2023-11-29 17:15:57 +0900

    feat: FlightInfoPage 생성 및 라우터 처리

---
+++ korean-air/src/pages/FlightInfoPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/@common/Header";
+
+const FlightInfoPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default FlightInfoPage;
