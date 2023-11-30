09ad93e25655c234f2e7dc2898974c0c5dc9e611
Soyoung Park <soyoung008@naver.com>
2023-11-30 18:13:20 +0900

    chore :불필요한 상수 삭제

--- korean-air/src/constants/constant.ts
+++ korean-air/src/constants/constant.ts
@@ -10,14 +10,6 @@ export interface calInfo {
   }[];
 }
 
-const NOTICE_TITLE = [
-  "국내선 유류할증료 (2023년 12월)",
-  "Rentalcars.com 제휴 서비스 일시 중단",
-  "마일리지 몰 KAL스토어 연말 할인 프로모션",
-  "사전 좌석 배정 서비스 신청 온라인 전환",
-];
-const NOTICE_DATE = ["2023.11.02", "2023.11.01", "2023.11.01", "2023.10.31"];
-
 const CALENDAR_INFO: calInfo[] = [
   {
     year: 2023,
