37e8db58d8a5314566e87248c3aa4d9dd56f8642
lydiacho <sheecho0804@ewhain.net>
2023-12-01 22:49:37 +0900

    style: finishPage 데탑뷰 흘러넘치는 것 수정

--- korean-air/src/pages/FinishPage.tsx
+++ korean-air/src/pages/FinishPage.tsx
@@ -29,9 +29,10 @@ const Wrapper = styled.section`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   padding: 1.6rem;
+  position: relative;
 
   & > header > svg {
     position: absolute;
